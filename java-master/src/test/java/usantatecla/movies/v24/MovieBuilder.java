f9cce13a7f0f1606e367d75b62370dcb86cf6fcf
Bastian <bastiantobar.94@gmail.com>
2024-04-06 22:27:35 +0200

    Se agregan Test v24

---
+++ java-master/src/test/java/usantatecla/movies/v24/MovieBuilder.java
@@ -0,0 +1,19 @@
+package usantatecla.movies.v23;
+
+public class MovieBuilder {
+
+	private String title;
+
+	public MovieBuilder() {
+		title = "movieName";
+	}
+	
+	public MovieBuilder title(String title) {
+		this.title = title;
+		return this;
+	}
+
+	public Movie build() {
+		return new Movie(title);
+	}
+}
