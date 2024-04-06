4f1c1858e9b760f4e29245aada4e39912dce8e6c
ricardo.vente <ricardo751991@gmail.com>
2024-04-06 19:50:48 +0200

    Eliminar el uso innecesario de la variable "each" en cada while

---
+++ java-master/src/main/java/usantatecla/movies/v22/Customer.java
@@ -0,0 +1,55 @@
+package usantatecla.movies.v22;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+	
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		rentals = new ArrayList<Rental>();
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		Iterator<Rental> rentals = this.rentals.iterator();
+		String result = "Rental Record for " + this.getName() + "\n";
+		while (rentals.hasNext()) {
+			result += "\t" + rentals.next().getMovie().getTitle() + "\t" + String.valueOf(rentals.next().getCharge()) + "\n";
+		}
+		result += "Amount owed is " + String.valueOf(this.getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(this.getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Iterator<Rental> rentals = this.rentals.iterator();
+		while (rentals.hasNext()) {
+			result += rentals.next().getCharge();
+		}
+		return result;
+	}
+	
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Iterator<Rental> rentals = this.rentals.iterator();
+		while (rentals.hasNext()) {
+			result += rentals.next().getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+}
